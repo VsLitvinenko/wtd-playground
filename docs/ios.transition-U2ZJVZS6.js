fc73d58204eb8bd63cfb15c980f0a2efe8ef88a9
VsLitvinenko <alorsl@live.com>
2025-07-17 16:39:02 +0300

    angular json fixes

---
+++ docs/ios.transition-U2ZJVZS6.js
@@ -0,0 +1,15 @@
+import {
+  iosTransitionAnimation,
+  shadow
+} from "./chunk-SAOJYTSX.js";
+import "./chunk-O55AJYKI.js";
+import "./chunk-ZMR2WFPN.js";
+import "./chunk-GZ5BDCOT.js";
+import "./chunk-HUY7ESWV.js";
+import "./chunk-GXFEW35R.js";
+import "./chunk-UL2P3LPA.js";
+export {
+  iosTransitionAnimation,
+  shadow
+};
+//# sourceMappingURL=ios.transition-U2ZJVZS6.js.map
---
+++ docs/md.transition-43P4K3UP.js
@@ -0,0 +1,13 @@
+import {
+  mdTransitionAnimation
+} from "./chunk-NLH4QFQF.js";
+import "./chunk-O55AJYKI.js";
+import "./chunk-ZMR2WFPN.js";
+import "./chunk-GZ5BDCOT.js";
+import "./chunk-HUY7ESWV.js";
+import "./chunk-GXFEW35R.js";
+import "./chunk-UL2P3LPA.js";
+export {
+  mdTransitionAnimation
+};
+//# sourceMappingURL=md.transition-43P4K3UP.js.map
